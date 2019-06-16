cede7042a958fb047229a7e2a28a4940c826d7ec
sharmishah <51690833+sharmishah@users.noreply.github.com>
2019-06-16 09:41:55 +0530

    Update isomorp.py

--- code kata/isomorp.py
+++ code kata/isomorp.py
@@ -1,12 +1,12 @@
 K,J=map(str,input().split())
-le=len(s1)
+le=len(K)
 d={}
 c=0
 for V in range(le):
     if(K[V] not in d.keys()):
-        d[K[i]]=J[i]
+        d[K[V]]=J[V]
     else:
-        if(d[s1[i]]==s2[i]):
+        if(d[K[V]]==J[V]):
             continue
         else:
             c=1
