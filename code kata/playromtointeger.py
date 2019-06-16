b40e68f02bfc36f69ee1634a103fbc88ad2ac851
sharmishah <51690833+sharmishah@users.noreply.github.com>
2019-06-16 09:25:49 +0530

    Create playromtointeger.py

---
+++ code kata/playromtointeger.py
@@ -0,0 +1,5 @@
+J =(input())
+O=["Zero","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+for V in range(21):
+  if k==O[V]:
+    print(V)
