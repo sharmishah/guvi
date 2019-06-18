e1f2be16b2fd25ea80f54121f3870acab0c2727f
sharmishah <51690833+sharmishah@users.noreply.github.com>
2019-06-18 18:32:37 +0530

    Update playos1schar.py

--- code kata/playos1schar.py
+++ code kata/playos1schar.py
@@ -1,27 +1,26 @@
-M=int(input())
-li=[]
-c=0 
-for i in range(M):
-    li.append(list(map(int,input().split())))
-if M==1 and  li[0]==[1]:
-     print('1')
-else:
-    for i in range(M):
-        for j in range(M):
-            if li[i][j]==1:
-                if i==0 and j==M-1:
-                    if(li[i][j-1]==0 and li[i+1][j]==0):
-                            c=c+1
-                elif i==0 and j==0:
-                    if li[i][j+1]==0 and li[i+1][j]==0:
-                        c=c+1
-                elif i==0 and li[i][j-1]==0 and li[i][j+1]==0 and li[i+1][j]==0:
-                    c=c+1
-                elif i==M-1 and j==M-1:
-                    if li[i-1][j]==0 and li[i][j-1]==0:
-                        c=c+1
-                elif i==n-1 and li[i][j-1]==0 and li[i][j+1]==0 and li[i-1][j]==0:
-                    c=c+1
-                elif i!=0 or i!=n-1 and (li[i][j+1]==0 and li[i+1][j]==0 and li[i-1][j]==0 and li[i][j-1]==0):
-                    c=c+1
-    print(c)    
+N=int(input())
+l=[]
+for i in range(0,N):
+    l.append(list(map(int,input().split())))
+c=0
+k=0
+for i in range(0,N):
+    for j in range(0,N):
+        if l[i][j]==1:
+            if i!=N-1 and l[i+1][j]==0:
+                c=c+1
+            if j!=N-1 and l[i][j+1]==0:
+                c=c+1
+            if i!=0 and l[i-1][j]==0:
+                c=c+1
+            if j!=0 and l[i][j-1]==0:
+                c=c+1
+            if i==0 and j==0 or i==N-1 and j==N-1  or i==0 and j==N-1 or i==N-1 and j==0 and c==2:
+                k=k+1
+            elif i==1 and j>0 and j<N-1 and c==3:
+                k=k+1
+            elif c==4:
+                k=k+1
+        c=0
+                
+print(k)
