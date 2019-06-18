6ac1901ad4a05970c1349b5de146fff88bc1d1a0
sharmishah <51690833+sharmishah@users.noreply.github.com>
2019-06-18 18:45:59 +0530

    Update playrepeatedlettr.py

--- code kata/playrepeatedlettr.py
+++ code kata/playrepeatedlettr.py
@@ -1,7 +1,19 @@
-import string
-B=input()
-B.split()
-B=B.replace(" ","")
-D=[i for i in B if B.count(i)==1]
-c=' '.join(D)
-print(c.lower())
+import sys,string, math,itertools
+
+def minChCnt(s) :
+    dic1 = {}
+    for c in s :
+        if not c.isspace() :
+            dic1[c] = dic1.get(c,0) + 1
+    min1 = sys.maxsize
+    L = []
+    min1 = min(dic1.values())
+    for k,v in dic1.items() :
+        if v == min1 :
+            L.append(k)
+    return L
+
+s = input()
+n = len(s)
+L = minChCnt(s)
+print(*L)
