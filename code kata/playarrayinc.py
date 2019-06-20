cfa28397a5094cda4440e61c61615366e4e156fb
sharmishah <51690833+sharmishah@users.noreply.github.com>
2019-06-20 20:39:24 +0530

    Update playarrayinc.py

--- code kata/playarrayinc.py
+++ code kata/playarrayinc.py
@@ -1,6 +1,12 @@
-K=int(input())
-pi=list(map(int,input().split()))
-gh=sorted(pi)
-wv=[]
-for i in range(0,k): wv.append(pi.index(gh[i])+1)
-print(*wv)
+nv=int(input())
+li=list(map(int,input().split()))
+s=sorted(l)
+l1s=[]
+for i in range(0,len(li)):
+    for j in range(0,len(li)):
+        if li[i]==s[j]:
+            l1s.append(j)
+            
+for i in range(len(l1s)):
+	l1s[i]=l1s[i]+1
+print(*l1s)
