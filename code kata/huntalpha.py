7d1b0f553d025b34668bb4536a924c1799a7396b
sharmishah <51690833+sharmishah@users.noreply.github.com>
2019-06-23 20:22:09 +0530

    Update huntalpha.py

--- code kata/huntalpha.py
+++ code kata/huntalpha.py
@@ -1,21 +1,11 @@
-import sys, string, math
-
-def Count_ch(L1, n1) :
-    count = [0] * (n1+1)
-    count[0] = 1
-    count[1] = 1
-    for i in range(2,n1+1) :
-        count[i] = 0
-        if (L1[i-1] > '0') :
-            count[i] = count[i-1]
-        if (L1[i-2] == '1'  or ( L1[i-2] == '2' and L1[i-1] < '7') ) :
-            count[i] += count[i-2]
-    return count[n]
-
-s1 = input()
-L1 = list(s1)
-if s1 == '101' :
-    print(2)
-    sys.exit()
-ans = Count_ch(L1,len(L1))
-print(ans)
+number=input()
+k2=1
+for i in range(len(number)-1):
+    sts=number[i]+number[i+1]
+    p=int(sts)
+    if p<=26 and number[i]!="0":
+        k2+=1
+if k2==3:
+    print(k2)
+else:
+    print(k2+(k2-1)//2)
