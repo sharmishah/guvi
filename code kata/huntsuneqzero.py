6eaccf1ad0ee5efacd3e55d687b631f95b670c98
sharmishah <51690833+sharmishah@users.noreply.github.com>
2019-06-23 20:45:03 +0530

    Update huntsuneqzero.py

--- code kata/huntsuneqzero.py
+++ code kata/huntsuneqzero.py
@@ -1,10 +1,11 @@
-xf=int(input())
-yf=list(map(int,input().split()))
-lf=max(yf)
-ej,f=0,0
-for i in range(0,len(yf)):
-  for j in range(i+1,len(yf)):
-    if abs(yf[i]+yf[j])<l:
-      ej,f=yf[i],yf[j]
-      lf=abs(ej+f)
-print(ej,f)
+n1=int(input())
+arra=list(map(int,input().split()))
+c=len(arra)
+large=max(arra)
+y,z=0,0
+for i in range(0,c-1):
+    for j in range(i+1,c):
+        if abs(arra[i]+arra[j])< large:
+            y,z=arra[i],arra[j]
+            large=abs(y+z)
+print(y, z)
