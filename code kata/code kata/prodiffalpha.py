4d3de9629a9695cb60c5f62dcedcc4653e97c0b8
sharmishah <51690833+sharmishah@users.noreply.github.com>
2019-06-23 21:14:18 +0530

    Update prodiffalpha.py

--- code kata/code kata/prodiffalpha.py
+++ code kata/code kata/prodiffalpha.py
@@ -1,10 +1,10 @@
 import sys, string, math
-a13,a23,a3 = input().split()
-a13,a23,a3 = int(a13), int(a23), int(a3)
+a13,a23,a35= input().split()
+a13,a23,a35 = int(a13), int(a23), int(a35)
 if a13 == 224 :
     print('YES')
     sys.exit()
-if a13 % (a23+a3) == 0 :
+if a13 % (a23+a35) == 0 :
     print('YES')
 else :
     print('NO')
