98c22e246730e860bd862231502f7accf2cf50e1
sharmishah <51690833+sharmishah@users.noreply.github.com>
2019-06-24 11:35:23 +0530

    Update playeratleastonevow.py

--- code kata/playeratleastonevow.py
+++ code kata/playeratleastonevow.py
@@ -1,1 +1,20 @@
-
+nn,kk=map(int,input().split())
+ll=[]
+aa=['a','e','i','o','u']
+cde=1
+for i in range(nn):
+	ss=input()
+	ll.append(ss)
+	if kk!=0:
+		for i in ss:
+			if i in aa:
+				cde=1
+				break
+			else:
+				cde=0
+		kk=kk-1
+	    
+if cde==1:
+    print("yes")
+else:
+    print("no")
