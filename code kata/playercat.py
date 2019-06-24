fb6e913d3865cf6c18a3183852c33631565e5b42
sharmishah <51690833+sharmishah@users.noreply.github.com>
2019-06-24 21:00:11 +0530

    Update playercat.py

--- code kata/playercat.py
+++ code kata/playercat.py
@@ -1,15 +1,15 @@
-def catalan(nn):
-    if nn<=1:
+def catalan(nnn):
+    if nnn<=1:
         return 1
     else:
-        ss=0
-        for i in range(nn):
-            ss=ss+catalan(i)*catalan(nn-i-1)
-        return ss
-nn=int(input())
-if nn==0:
-    nn=1
+        sss=0
+        for i in range(nnn):
+            sss=sss+catalan(i)*catalan(nnn-i-1)
+        return sss
+nnn=int(input())
+if nnn==0:
+    nnn=1
 c=[]
-for i in range(nn):
+for i in range(nnn):
     c.append(catalan(i))
 print(*c)
