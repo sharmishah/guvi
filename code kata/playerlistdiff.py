c165568e3670d0ecd391186bc43c1c5918339d48
sharmishah <51690833+sharmishah@users.noreply.github.com>
2019-06-24 12:33:12 +0530

    Update playerlistdiff.py

--- code kata/playerlistdiff.py
+++ code kata/playerlistdiff.py
@@ -1,21 +1,21 @@
-ss=input()
-rr=input()
-gg=[]
-if (ss.isalpha() or " " in ss) and (rr.isalpha() or " " in rr):
-    ss=list(ss.split(" "))
-    rs=list(rr.split(" "))
-    for i in ss:
-        if ss.count(i) > rr.count(i) and i not in gg:
-            gg.append(i)
-    for i in rr:
-        if rr.count(i)>ss.count(i) and i not in gg:
-            gg.append(i)
-    print(*gg)
+vs=input()
+vr=input()
+vg=[]
+if (vs.isalpha() or " " in vs) and (vr.isalpha() or " " in vr):
+    vs=list(vs.split(" "))
+    vr=list(vr.split(" "))
+    for i in vs:
+        if vs.count(i) > vr.count(i) and i not in vg:
+            vg.append(i)
+    for i in vr:
+        if vr.count(i)>vs.count(i) and i not in vg:
+            vg.append(i)
+    print(*vg)
 else:
-    for i in ss:
-        if ss.count(i)>rr.count(i) and i not in gg:
-            gg.append(i)
-    for i in rr:
-        if rr.count(i)>ss.count(i) and i not in gg:
-            gg.append(i)
-    print(*gg)
+    for i in vs:
+        if vs.count(i)>vr.count(i) and i not in vg:
+            vg.append(i)
+    for j in vr:
+        if vr.count(j)>vs.count(j) and j not in vg:
+            vg.append(j)
+    print(*vg)
