612956930aa413aa1dcff0712410f0eba742753d
sharmishah <51690833+sharmishah@users.noreply.github.com>
2019-06-24 18:47:49 +0530

    Update playerrghtang.py

--- code kata/playerrghtang.py
+++ code kata/playerrghtang.py
@@ -1,5 +1,5 @@
-aa,bb,cc=map(int,input().split())
-if (aa**2)+(bb**2)==cc**2 or (bb**2)+(cc**2)==aa**2 or (aa**2)+(cc**2)==bb**2:
+aaa,bbb,ccc=map(int,input().split())
+if (aaa**2)+(bbb**2)==ccc**2 or (bbb**2)+(ccc**2)==aaa**2 or (aaa**2)+(ccc**2)==bbb**2:
 	print("yes")
 else:
 	print("no")
