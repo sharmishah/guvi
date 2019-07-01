24f1e006b8999ce4d4cf07eed0cfafdf961fd594
sharmishah <51690833+sharmishah@users.noreply.github.com>
2019-07-01 21:14:37 +0530

    Update huntercount.py

--- code kata/huntercount.py
+++ code kata/huntercount.py
@@ -1,17 +1,13 @@
-import sys, string, math, itertools
-def isprime(nkn) :
-    if nkn == 1 : return False
-    if nkn == 2: return True
-    for i in range(2,nkn) :
-        if nkn%i == 0 : return False
-    return True
-
-aa,bb = input().split()
-aa = int(aa)
-bb = int(bb)
-cnt = 0
-for i in range(aa,bb+1) :
-    ss = bbin(i)[2:]
-    kk = ss.count('1')
-    if isprime(kk) : cnt += 1
-print(cnt)
+sd1,sd2=map(int,input().split())
+sd=0
+for i in range(sd1,sd2+1):
+    xd=bin(i)
+    xd=xd[2:len(xd)]
+    xd=xd.count("1")
+    sd2=0
+    for i in range(1,xd):
+        if xd%i==0:
+            sd2=sd2+1
+    if sd2==1:
+        sd=sd+1
+print(sd)
